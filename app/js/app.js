6476129149a535b3de52fe49ed14672bbf288272
Tim <nebukam@gmail.com>
2021-01-17 16:27:21 -0500

    WIP

--- app/js/app.js
+++ app/js/app.js
@@ -2,11 +2,21 @@
 
 const AppBase = require(`@nkm/app-core`);
 
-class App extends AppBase {
+class MyAppClassName extends AppBase {
     constructor() { super(); }
 
+    _Init() {
+        super._Init();
 
+        this._defaultUserPreferences = {
+            aProperty: `aValue`,
+            anObject: {
+                aSubProperty: `aSubValue`
+            },
+            anArray: []
+        };
+    }
 
 }
 
-module.exports = App;
+module.exports = MyAppClassName;
--- infos.md
+++ infos.md
@@ -1,5 +1,6 @@
 Search and replace :
 
 'app-name-replace-me' - Short app name (A-Z a-z 0-9)
-'App Name : Replace me' - Cosmetic app name
-'company-name-replace-me' - Company name
+'App Name : Replace me' - Cosmetic app name (app title etc)
+'company-name-replace-me' - Company name 
+'MyAppClassName' - Your browser app class name (in js/app.js)
